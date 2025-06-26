cc03875d12ab61827813ec476827e571ab3b5003
xmodify <xmodifya@gmail.com>
2025-06-27 02:22:48 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'HTPReport') }}</title>
+    <title>{{ config('app.name', 'H-SmartData') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
