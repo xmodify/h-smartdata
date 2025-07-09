8155258422f0313221ea1ea878d625a461bd4a1d
xmodify <xmodifya@gmail.com>
2025-07-10 00:15:14 +0700

    update

--- app/Http/Controllers/Dashboard_DigitalhealthController.php
+++ app/Http/Controllers/Dashboard_DigitalhealthController.php
@@ -263,7 +263,7 @@ public function opd_mornitor(Request $request )
 		LEFT JOIN health_med_service hm ON hm.vn=o.vn
         LEFT JOIN htp_report.nhso_endpoint ep ON ep.personalId=v.cid AND DATE(ep.claimDate)=o.vstdate AND ep.claimStatus="E"
         LEFT JOIN htp_report.nhso_endpoint_indiv epi ON epi.cid=v.cid AND DATE(epi.serviceDateTime)=o.vstdate
-        LEFT JOIN htp_report.nhso_endpoint_indiv epi_homeward ON epi_homeward.cid=pt.cid 
+        LEFT JOIN htp_report.nhso_endpoint_indiv epi_homeward ON epi_homeward.cid=v.cid 
             AND DATE(epi_homeward.serviceDateTime)=o.vstdate AND epi_homeward.claimType = "PG0140001" 
         WHERE o.vstdate = DATE(NOW()) GROUP BY o.vn) AS a');
 
