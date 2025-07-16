d84c2ab845c0595aa70d8dae50e0d4ab5b22ec57
xmodify <xmodifya@gmail.com>
2025-07-16 22:04:49 +0700

    update

--- app/Http/Controllers/Admin/MainSettingController.php
+++ app/Http/Controllers/Admin/MainSettingController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\MainSetting;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class MainSettingController extends Controller
 {
